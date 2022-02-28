57b32aa7fdc918ab7af6661ed3f5ca47d751df18
elnurxo <elnurykh@code.edu.az>
2022-02-28 19:36:34 +0400

    mobile-accordion-for-categories

--- FinalProject Drizly/DrizzyFrontEnd/assets/Js/main.js
+++ FinalProject Drizly/DrizzyFrontEnd/assets/Js/main.js
@@ -1,4 +1,5 @@
 $(function () {   
+    //NAVBAR SCROLL
     $(document).scroll(function () {
         if ($(window).width() > 992) 
         {
@@ -15,6 +16,30 @@ $(function () {
             $("#navbar").addClass("scrollactive"); 
         }
     });
+    //CATEGORIES MOBILE ACCORDION
+    $(".categoriesmobile").click(function(){
+       if($(window).width() <= 480){
+        if($(this).next().css("display")=="none"){
+            $(this).next().css("display","block")
+            $(this).prev().find("i").addClass("fa-chevron-up")
+            $(this).prev().find("i").removeClass("fa-chevron-down")         
+            return;
+        }
+        else{
+            $(this).next().css("display","none")
+            $(this).prev().find("i").addClass("fa-chevron-down")
+            $(this).prev().find("i").removeClass("fa-chevron-up")  
+            return;
+        }
+       }
+       else{
+        $(this).next().css("display","block")
+       } 
+    });
+    if($(window).width() >= 480){
+        console.log("480den boyukdu")
+        $(".categoriesmobile").next().css("display","block")
+    }
 });
 // LOADER
 window.addEventListener("DOMContentLoaded", () => {
