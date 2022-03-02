9d66efbef10a4ade9a5619ff6195b5c2b791e715
elnurxo <elnurykh@code.edu.az>
2022-03-02 16:52:13 +0400

    Navbar,search ul done

---
+++ DrizzyFrontEnd/assets/Js/main.js
@@ -0,0 +1,92 @@
+$(function () {  
+    //NAVBAR SCROLL
+    $(document).scroll(function () {
+        if ($(window).width() > 992) 
+        {
+            if (window.scrollY > 36) 
+            {
+                $("#navbar").addClass("scrollactive");
+            }
+            else
+            {
+                $("#navbar").removeClass("scrollactive");
+            }
+        }
+        else{
+            $("#navbar").addClass("scrollactive"); 
+        }
+    });
+    //BRAND SLIDER
+    $('.brand-active-carousel').slick({
+		arrows: false,
+		slidesToShow: 4,
+        autoplay: true,
+        autoplaySpeed: 1000,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+    //CATEGORIES MOBILE ACCORDION
+    $(".categoriesmobile").click(function(){
+       if($(window).width() <= 480){
+        if($(this).next().css("display")=="none"){
+            $(this).next().css("display","block")
+            $(this).prev().find("i").addClass("fa-chevron-up")
+            $(this).prev().find("i").removeClass("fa-chevron-down")         
+            return;
+        }
+        else{
+            $(this).next().css("display","none")
+            $(this).prev().find("i").addClass("fa-chevron-down")
+            $(this).prev().find("i").removeClass("fa-chevron-up")  
+            return;
+        }
+       }
+    });
+    //MOBILE NAVBAR HAMBURGER CLICK
+    $(".search-mobile").click(function(){
+        console.log("jskfh");
+    })
+});
+// LOADER
+window.addEventListener("DOMContentLoaded", () => {
+    $("#loadingwrapper").css({
+      display: "block",
+      visibility: "visible",
+      opacity: "1",
+    });
+    $("body").css({
+        overflow: "hidden"
+    });
+    setTimeout(removeLoader, 900);
+  });
+  function removeLoader() {
+    $("#loadingwrapper").fadeOut(600, function () {
+      $("#loadingwrapper").remove();
+    });
+    $("body").css({
+        overflow: "visible"
+    });
+  }
+  //CATEGORIES DISPLAY BLOCK/NONE ON RESIZE
+  var onresize = function() {
+    width = document.body.clientWidth;
+    if(width>480){
+        $(".categoriesmobile").next().css("display","block")
+    }
+    else{
+        $(".categoriesmobile").next().css("display","none")
+    }
+ }
+ window.addEventListener("resize", onresize);
