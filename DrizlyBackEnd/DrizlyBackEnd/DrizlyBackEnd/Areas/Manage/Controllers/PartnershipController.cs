66714b6b98d62adc2864466a672f322b738c66d6
elnurxo <elnurykh@code.edu.az>
2022-03-17 15:51:16 +0400

    Partner create done

--- DrizlyBackEnd/DrizlyBackEnd/DrizlyBackEnd/Areas/Manage/Controllers/PartnershipController.cs
+++ DrizlyBackEnd/DrizlyBackEnd/DrizlyBackEnd/Areas/Manage/Controllers/PartnershipController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -27,6 +28,55 @@ namespace DrizlyBackEnd.Areas.Manage.Controllers
             string pageSizeStr = _context.Settings.FirstOrDefault(x => x.Key == "PageSize").Value;
             int pageSize = string.IsNullOrWhiteSpace(pageSizeStr) ? 3 : int.Parse(pageSizeStr);
             return View(PagenatedList<Partnership>.Create(_context.Partnerships.AsQueryable(), page, pageSize));
+        }
+        //CREATE ACTION
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Partnership partnership)
+        {
+            if (partnership.ImageFile == null)
+                ModelState.AddModelError("ImageFile", "Image file is required!");
+
+            if (!ModelState.IsValid)
+                return View();
+
+            if (partnership.ImageFile.ContentType != "image/jpeg" && partnership.ImageFile.ContentType != "image/png")
+            {
+                ModelState.AddModelError("ImageFile", "File type must be jpeg or png");
+                return View(partnership);
+            }
+
+            if (partnership.ImageFile.Length > 2097152)
+            {
+                ModelState.AddModelError("ImageFile", "file size must be less than 2mb");
+                return View(partnership);
+            }
+
+            string fileName = partnership.ImageFile.FileName;
+
+            if (fileName.Length > 64)
+            {
+                fileName = fileName.Substring(partnership.ImageFile.FileName.Length - 64, 64);
+            }
+
+            partnership.Image = (Guid.NewGuid().ToString() + (fileName));
+
+            string path = Path.Combine(_env.WebRootPath, "uploads/partnerships", partnership.Image);
+
+            using (FileStream stream = new FileStream(path, FileMode.Create))
+            {
+                partnership.ImageFile.CopyTo(stream);
+            }
+
+            partnership.CreatedAt = DateTime.UtcNow.AddHours(4);
+
+            _context.Partnerships.Add(partnership);
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
         }
     }
 }
