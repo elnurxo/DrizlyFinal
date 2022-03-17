50c3f1a9c88172ba27c781ffb7109de592f16137
elnurxo <elnurykh@code.edu.az>
2022-03-17 15:43:21 +0400

    Partners index done

---
+++ DrizlyBackEnd/DrizlyBackEnd/DrizlyBackEnd/Areas/Manage/Controllers/PartnershipController.cs
@@ -0,0 +1,32 @@
+﻿using DrizlyBackEnd.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrizlyBackEnd.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    //[Authorize(Roles = "SuperAdmin,Admin")]
+    public class PartnershipController : Controller
+    {
+        private readonly DrizlyContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public PartnershipController(DrizlyContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        //INDEX ACTION
+        public IActionResult Index(int page = 1)
+        {
+            string pageSizeStr = _context.Settings.FirstOrDefault(x => x.Key == "PageSize").Value;
+            int pageSize = string.IsNullOrWhiteSpace(pageSizeStr) ? 3 : int.Parse(pageSizeStr);
+            return View(PagenatedList<Partnership>.Create(_context.Partnerships.AsQueryable(), page, pageSize));
+        }
+    }
+}
