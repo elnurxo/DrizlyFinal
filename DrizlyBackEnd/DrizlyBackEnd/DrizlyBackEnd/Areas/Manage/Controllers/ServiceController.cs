1a0f54c59a62b3af2b70cc8468482a30f786691a
elnurxo <elnurykh@code.edu.az>
2022-03-17 15:08:10 +0400

    Service crud,done

---
+++ DrizlyBackEnd/DrizlyBackEnd/DrizlyBackEnd/Areas/Manage/Controllers/ServiceController.cs
@@ -0,0 +1,166 @@
+﻿using DrizlyBackEnd.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrizlyBackEnd.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    //[Authorize(Roles = "SuperAdmin,Admin")]
+    public class ServiceController : Controller
+    {
+        private readonly DrizlyContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public ServiceController(DrizlyContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        //INDEX ACTION
+        public IActionResult Index(int page = 1)
+        {
+            string pageSizeStr = _context.Settings.FirstOrDefault(x => x.Key == "PageSize").Value;
+            int pageSize = string.IsNullOrWhiteSpace(pageSizeStr) ? 3 : int.Parse(pageSizeStr);
+            return View(PagenatedList<Service>.Create(_context.Services.AsQueryable(), page, pageSize));
+        }
+        //CREATE ACTION
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Service service)
+        {
+            if (service.ImageFile == null)
+                ModelState.AddModelError("ImageFile", "Image file is required!");
+
+            if (!ModelState.IsValid)
+                return View();
+
+            if (service.ImageFile.ContentType != "image/jpeg" && service.ImageFile.ContentType != "image/png")
+            {
+                ModelState.AddModelError("ImageFile", "File type must be jpeg or png");
+                return View(service);
+            }
+
+            if (service.ImageFile.Length > 2097152)
+            {
+                ModelState.AddModelError("ImageFile", "file size must be less than 2mb");
+                return View(service);
+            }
+
+            string fileName = service.ImageFile.FileName;
+
+            if (fileName.Length > 64)
+            {
+                fileName = fileName.Substring(service.ImageFile.FileName.Length - 64, 64);
+            }
+
+            service.Image = (Guid.NewGuid().ToString() + (fileName));
+
+            string path = Path.Combine(_env.WebRootPath, "uploads/services", service.Image);
+
+            using (FileStream stream = new FileStream(path, FileMode.Create))
+            {
+                service.ImageFile.CopyTo(stream);
+            }
+
+
+            _context.Services.Add(service);
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+
+        //EDIT ACTION
+        public IActionResult Edit(int id)
+        {
+            Service service = _context.Services.FirstOrDefault(x => x.Id == id);
+
+            if (service == null) return NotFound();
+
+            return View(service);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Service service)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            Service existService = _context.Services.FirstOrDefault(x => x.Id == service.Id);
+            if (existService == null) return NotFound();
+            if (service.ImageFile != null)
+            {
+                if (service.ImageFile.ContentType != "image/jpeg" && service.ImageFile.ContentType != "image/png")
+                {
+                    ModelState.AddModelError("ImageFile", "file type must be jpeg or png");
+                    return View(service);
+                }
+
+                if (service.ImageFile.Length > 2097152)
+                {
+                    ModelState.AddModelError("ImageFile", "file size must be less than 2mb");
+                    return View(service);
+                }
+
+                string fileName = service.ImageFile.FileName;
+
+                if (fileName.Length > 64)
+                {
+                    fileName = fileName.Substring(service.ImageFile.FileName.Length - 64, 64);
+                }
+
+                service.Image = (Guid.NewGuid().ToString() + (fileName));
+
+                string path = Path.Combine(_env.WebRootPath, "uploads/services", service.Image);
+
+                using (FileStream stream = new FileStream(path, FileMode.Create))
+                {
+                    service.ImageFile.CopyTo(stream);
+                }
+
+                if (existService.Image != null)
+                {
+                    string existPath = Path.Combine(_env.WebRootPath, "uploads/services", existService.Image);
+                    if (System.IO.File.Exists(existPath))
+                        System.IO.File.Delete(existPath);
+                }
+
+                existService.Image = service.Image;
+            }
+
+            if (existService == null) return NotFound();
+            existService.Title = service.Title;
+            existService.Desc = service.Desc;
+
+            _context.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+        //DELETE ACTION
+        public IActionResult Delete(int id)
+        {
+            Service existService = _context.Services.FirstOrDefault(x => x.Id == id);
+
+            if (existService == null)
+                return NotFound();
+
+            _context.Services.Remove(existService);
+
+            string existPath = Path.Combine(_env.WebRootPath, "uploads/services", existService.Image);
+            if (System.IO.File.Exists(existPath))
+                System.IO.File.Delete(existPath);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+    }
+}
--- DrizlyBackEnd/DrizlyBackEnd/DrizlyBackEnd/wwwroot/assets/Js/sweetalert.js
+++ DrizlyBackEnd/DrizlyBackEnd/DrizlyBackEnd/wwwroot/assets/Js/sweetalert.js
@@ -1,4 +1,41 @@
 ﻿$(document).ready(function () {
+    //DELETE-FEATURE
+    $(document).on("click", ".delete-service", function (e) {
+        e.preventDefault();
+        console.log("alkhgfaslg");
+        let path = $(this).attr("href")
+
+        Swal.fire({
+            title: 'Are You Sure?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Delete!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(path).then(Response => {
+                    if (Response.ok) {
+                        Swal.fire(
+                            'Deleted!',
+                            'Selected File Deleted Successfully',
+                            'success'
+                        ).then(function () {
+                            location.reload();
+                        })
+                    }
+                    else {
+                        Swal.fire(
+                            'Failed!',
+                            'Selected File Deleted Successfully',
+                            'success'
+                        )
+                        location.reload();
+                    }
+                })
+            }
+        })
+    })
     //LOG-OUT USER
     $(document).on("click", ".logout-user", function (e) {
         e.preventDefault();
