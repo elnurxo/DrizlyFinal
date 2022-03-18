d4790f7c7967c7fcf00883e2e5ec1cdf7d321d8e
elnurxo <elnurykh@code.edu.az>
2022-03-18 16:16:20 +0400

    ShopViewonBackEnd

---
+++ DrizlyBackEnd/DrizlyBackEnd/DrizlyBackEnd/Controllers/ShopController.cs
@@ -0,0 +1,24 @@
+﻿using DrizlyBackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrizlyBackEnd.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly DrizlyContext _context;
+        public ShopController(DrizlyContext context)
+        {
+            _context = context;
+        }
+
+        //INDEX ACTION
+        public IActionResult Index()
+        { 
+            return View();
+        }
+    }
+}
---
+++ DrizlyBackEnd/DrizlyBackEnd/DrizlyBackEnd/wwwroot/assets/Js/slickslider.js
@@ -0,0 +1,62 @@
+﻿$(function () {
+	//BRAND SLIDER
+	$('.brand-active-carousel').slick({
+		arrows: false,
+		slidesToShow: 4,
+		autoplay: true,
+		autoplaySpeed: 1000,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+	//PRODUCTS-SHOP-SLIDER
+	$('.drink-slider').slick({
+		arrows: true,
+		infinite: true,
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 1200,
+		responsive: [
+			{
+				breakpoint: 1400,
+				settings: {
+					arrows: false
+				}
+			},
+
+			{
+				breakpoint: 992,
+				settings: {
+					arrows: false,
+					slidesToShow: 4
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					arrows: false,
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+});
